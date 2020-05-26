c8df72d6a1a7fe0b8ea52c6e620c1cdc6b9264eb
roningrum <rizkioktavianingrum@gmail.com>
2020-05-26 11:33:41 +0700

    update statefullwidget

---
+++ lib/statefullwidget.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+//membuat aplikasi counter
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int number = 0;
+
+  //method tekan tombol
+  void tekanTombol() {
+    //untuk set state dan refresh tampilan
+    setState(() {
+      number = number + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Statefull Widget Demo"),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                number.toString(),
+                style: TextStyle(fontSize: 10 + number.toDouble()),
+              ),
+              RaisedButton(
+                child: Text("Tambah Bilangan"),
+                onPressed: tekanTombol,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
