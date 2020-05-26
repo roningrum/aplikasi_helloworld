b59f62ea29ab2340949184c9a0166a2d155d9f85
roningrum <rizkioktavianingrum@gmail.com>
2020-05-26 11:55:48 +0700

    update anonymous

---
+++ lib/anonymousmethod.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String message = "Ini adalah Text";
+
+  //anonympus method
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("AnonymousMethod"),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(message),
+              RaisedButton(
+                child: Text("Tekan Saya"),
+                onPressed: () {
+                  setState(() {
+                    message = " Tombol sudah ditekan";
+                  });
+                },
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
